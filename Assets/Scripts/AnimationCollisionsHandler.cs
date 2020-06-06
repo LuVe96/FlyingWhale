11b2cfece401b5d62b28dffa14ff03e098f94921
LuVe96 <LukiVe16@gmail.com>
2020-06-06 20:46:33 +0200

    added fish sprite animation

---
+++ Assets/Scripts/AnimationCollisionsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCollisionsHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private PolygonCollider2D[] colliders;
+    private int currentColliderIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetColliderForSprite(int spriteNum)
+    {
+        colliders[currentColliderIndex].enabled = false;
+        currentColliderIndex = spriteNum;
+        colliders[currentColliderIndex].enabled = true;
+    }
+}
