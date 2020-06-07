7a5e91396c78fb2d0d028fe495d1bbb52ce8a938
LuVe96 <LukiVe16@gmail.com>
2020-06-07 11:41:10 +0200

    create file for leveldificulty enum, make fish bit faster

---
+++ Assets/Scripts/LevelDifficultyEnum.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    public enum LevelDifficulty
+    {
+        Easy,
+        Normal,
+        Hard
+    }
+
+    // extension für enum: PlayerSpeed
+    static class LevelDifficultyMethods
+    {
+        // here define how many obj the levelgenerat uses on each dificulty
+        public static Dictionary<string, int> getNumberofObj(this LevelDifficulty d1)
+        {
+            Dictionary<string, int> amountOfObjDic = new Dictionary<string, int>();
+            amountOfObjDic.Add("numberOfFish", 0);
+            amountOfObjDic.Add("numberOfRainCloud", 0);
+            amountOfObjDic.Add("numberOfSunnyCloud", 0);
+            switch (d1)
+            {
+                case LevelDifficulty.Easy:
+                    amountOfObjDic["numberOfFish"] = 30;
+                    amountOfObjDic["numberOfRainCloud"] = 10;
+                    amountOfObjDic["numberOfSunnyCloud"] = 20;
+                    return amountOfObjDic;
+                case LevelDifficulty.Normal:
+                    amountOfObjDic["numberOfFish"] = 20;
+                    amountOfObjDic["numberOfRainCloud"] = 10;
+                    amountOfObjDic["numberOfSunnyCloud"] = 30;
+                    return amountOfObjDic;
+                case LevelDifficulty.Hard:
+                    amountOfObjDic["numberOfFish"] = 10;
+                    amountOfObjDic["numberOfRainCloud"] = 30;
+                    amountOfObjDic["numberOfSunnyCloud"] = 20;
+                    return amountOfObjDic;
+                default:
+                    return amountOfObjDic;
+
+            }
+        }
+    }
