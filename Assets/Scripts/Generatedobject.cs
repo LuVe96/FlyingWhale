9407076df643b62d523a006b91a8c093dcde27e1
LuVe96 <LukiVe16@gmail.com>
2020-06-17 15:07:51 +0200

    adjusted prefabs

--- Assets/Scripts/Generatedobject.cs
+++ Assets/Scripts/Generatedobject.cs
@@ -20,12 +20,12 @@ public class Generatedobject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
-        {
-            float randomX = Random.Range(10.0f, 20.0f);
-            float randomY = Random.Range(-10.0f, 10.0f);
-            transform.position = new Vector3(randomX, randomY + referenz.position.y, 0);
-            transform.GetChild(0).GetComponent<PolygonCollider2D>().enabled = true;
-        }
+        //if (transform.position.x < -10)
+        //{
+        //    float randomX = Random.Range(10.0f, 20.0f);
+        //    float randomY = Random.Range(-10.0f, 10.0f);
+        //    transform.position = new Vector3(randomX, randomY + referenz.position.y, 0);
+        //    transform.GetChild(0).GetComponent<PolygonCollider2D>().enabled = true;
+        //}
     }
 }
---
+++ Assets/TilesGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesGenerator : MonoBehaviour
+{
+
+    public Transform referenz;
+    public GameObject easyTiles;
+
+    private int numEasyTiles = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numEasyTiles = easyTiles.transform.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int num = Random.Range(0, numEasyTiles - 1);
+        float randomY = Random.Range(-5, 5);
+        GameObject obj = Instantiate(easyTiles.transform.GetChild(num).gameObject);
+        obj.transform.position = new Vector3(30, randomY, 0);
+
+    }
+}
