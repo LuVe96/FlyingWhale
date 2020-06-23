ca5569a873b424dac1e215aaaadcde07267bb817
LuVe96 <LukiVe16@gmail.com>
2020-06-23 22:14:18 +0200

    added paralax background and background video

---
+++ Assets/TwoPartBgRepeater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPartBgRepeater : MonoBehaviour
+{
+
+    public GameObject secondBg;
+    public bool isSet { get; private set; } = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( transform.position.x <= 0 && !secondBg.GetComponent<TwoPartBgRepeater>().isSet)
+        {
+            secondBg.transform.position = new Vector3 (53, 0, 0);
+            secondBg.GetComponent<TwoPartBgRepeater>().setIsSet(true);
+            setIsSet(false);
+
+        }
+    }
+
+ 
+    public void setIsSet(bool _isSet)
+    {
+
+        isSet = _isSet;
+
+    }
+}
---
+++ Assets/VideoCustomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCustomizer : MonoBehaviour
+{
+
+    private float playTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playTime = GameManager.Instance.timeForEachDificulty * 3.5f;
+        GetComponent<UnityEngine.Video.VideoPlayer>().playbackSpeed = 1 / playTime * 15;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
